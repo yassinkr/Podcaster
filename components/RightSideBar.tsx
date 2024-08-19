f2cca659d7cb49292259775c43a2dad69495a357
yassinkr <kerraiyassin@gmail.com>
2024-08-19 18:57:21 +0100

    fixed display users

--- components/RightSideBar.tsx
+++ components/RightSideBar.tsx
@@ -6,13 +6,16 @@ import React from 'react'
 import Image from 'next/image';
 import Header from './Header';
 import Carousel from './Carousel';
+import { Loader } from 'lucide-react';
 
 const RightSideBar = async () => {
   const user = auth();
   const topUsers = await getTopUserByPodcastCount();
   const podcasts= await getPodcasts();
   if (user.userId) var userData= await getUserByClerkId(user.userId);
-  
+  if(!topUsers || !podcasts) return(<div className='w-full h-screen flex justify-center items-center'>
+    <Loader size={30} className="animate-spin  text-orange-1"/>
+    </div>)
   return (
     <section className='right_sidebar text-white-1'>
 
@@ -41,9 +44,20 @@ const RightSideBar = async () => {
       <Header headerTitle='Top Podcasters' titleClassName=''/>
       <div className=' flex flex-col gap-6'>
        {topUsers?.slice(0,4).map((podcaster)=>(
-        <Link href={`/profile/${podcaster.clerkId}`}>
-        <div key={podcaster.id} className='flex cursor-pointer justify-between'>
-        
+        <Link href={`/profile/${podcaster.clerkId}`} key={podcaster.id}>
+        <div  className='flex cursor-pointer justify-between'>
+        <div className='flex gap-3 justify-start items-centers'>
+        <Image
+          src={podcaster.image}
+          alt={podcaster.name}
+          width={44}
+          height={44}
+          className='rounded-lg aspect-square'/>
+          <h2 className='text-sm text-white-1 font-semibold'>{podcaster.name}</h2>
+          </div>
+          <div className='flex items-center'>
+          <p className='text-xs font-normal'>{podcaster.podcastCount} podcasts</p>
+          </div>
         </div>
         </Link>
        ))}
--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,9 @@ const nextConfig = {
                 {protocol: 'https',
         hostname: 'lovely-flamingo-139.convex.cloud',},
         {protocol: 'https',
-            hostname: 'utfs.io',}
+            hostname: 'utfs.io',},
+            {protocol: 'https',
+                hostname: 'img.clerk.com',}
         ],
           
     },
