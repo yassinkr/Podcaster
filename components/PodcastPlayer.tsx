dbb60034ddd63bd05e24d5fd3c6dd35081cff773
yassinkr <kerraiyassin@gmail.com>
2024-08-29 23:19:50 +0100

    finished the audio player

--- components/PodcastPlayer.tsx
+++ components/PodcastPlayer.tsx
@@ -1,20 +1,184 @@
 "use client";
-import { cn } from '@/lib/utils'
+import React, { useRef, useState, useEffect } from 'react';
+import Image from 'next/image';
 import { useAudio } from '@/providers/AudioProvider';
-import React from 'react'
-import AudioPlayer from './AudioPlayer';
+import { Progress } from "@/components/ui/progress"
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
 
 const PodcastPlayer = () => {
-    const {audio} = useAudio();
-    console.log("🚀 ~ PodcastPlayer ~ audio:", audio)
- if(!audio?.audioUrl) return null;
-    return (
-    <div className={cn("sticky bottom-0 left-0 flex size-full flex-col",
-        { "hidden": !audio?.audioUrl }
-    )}>
-     <AudioPlayer audioUrl={audio.audioUrl} userPodcast={undefined} />
-    </div>
-  )
-}
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const [isMuted, setIsMuted] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const { audio } = useAudio();
 
-export default PodcastPlayer
+  const togglePlayPause = () => {
+    if (audioRef.current?.paused) {
+      audioRef.current?.play();
+      setIsPlaying(true);
+    } else {
+      audioRef.current?.pause();
+      setIsPlaying(false);
+    }
+  };
+  const toggleMute = () => {
+    if (audioRef.current) {
+      audioRef.current.muted = !isMuted;
+      setIsMuted((prev) => !prev);
+    }
+  };
+
+  const forward = () => {
+    if (
+      audioRef.current &&
+      audioRef.current.currentTime &&
+      audioRef.current.duration &&
+      audioRef.current.currentTime + 5 < audioRef.current.duration
+    ) {
+      audioRef.current.currentTime += 5;
+    }
+  };
+
+  const rewind = () => {
+    if (audioRef.current && audioRef.current.currentTime - 5 > 0) {
+      audioRef.current.currentTime -= 5;
+    } else if (audioRef.current) {
+      audioRef.current.currentTime = 0;
+    }
+  };
+
+  useEffect(() => {
+    const updateCurrentTime = () => {
+      if (audioRef.current) {
+        setCurrentTime(audioRef.current.currentTime);
+      }
+    };
+
+    const audioElement = audioRef.current;
+    if (audioElement) {
+      audioElement.addEventListener("timeupdate", updateCurrentTime);
+
+      return () => {
+        audioElement.removeEventListener("timeupdate", updateCurrentTime);
+      };
+    }
+  }, []);
+
+  useEffect(() => {
+    const audioElement = audioRef.current;
+    if (audio?.audioUrl) {
+      if (audioElement) {
+        audioElement.play().then(() => {
+          setIsPlaying(true);
+        });
+      }
+    } else {
+      audioElement?.pause();
+      setIsPlaying(true);
+    }
+  }, [audio]);
+  const handleLoadedMetadata = () => {
+    if (audioRef.current) {
+      setDuration(audioRef.current.duration);
+    }
+  };
+
+  const handleAudioEnded = () => {
+    setIsPlaying(false);
+  };
+  const formatTime = (timeInSeconds: number) => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = Math.floor(timeInSeconds % 60);
+    return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+  };
+
+  return (
+    <div
+      className={cn("sticky bottom-0 left-0 flex size-full flex-col", {
+        hidden: !audio?.audioUrl || audio?.audioUrl === "",
+      })}
+    >
+      {/* change the color for indicator inside the Progress component in ui folder */}
+      <Progress
+        value={(currentTime / duration) * 100}
+        className="w-full"
+        max={duration}
+      />
+      <section className="glassmorphism-black flex h-[112px] w-full items-center justify-between px-4 max-md:justify-center max-md:gap-5 md:px-12">
+        <audio
+          ref={audioRef}
+          src={audio?.audioUrl}
+          className="hidden"
+          onLoadedMetadata={handleLoadedMetadata}
+          onEnded={handleAudioEnded}
+        />
+        <div className="flex items-center gap-4 max-md:hidden">
+          <Link href={`/podcast/${audio?.podcastId}`}>
+            <Image
+              src={audio?.imageUrl! || "/images/player1.png"}
+              width={64}
+              height={64}
+              alt="player1"
+              className="aspect-square rounded-xl"
+            />
+          </Link>
+          <div className="flex w-[160px] flex-col">
+            <h2 className="text-14 truncate font-semibold text-white-1">
+              {audio?.title}
+            </h2>
+            <p className="text-12 font-normal text-white-2">{audio?.author}</p>
+          </div>
+        </div>
+        <div className="flex-center cursor-pointer gap-3 md:gap-6">
+          <div className="flex items-center gap-1.5">
+            <Image
+              src={"/icons/reverse.svg"}
+              width={24}
+              height={24}
+              alt="rewind"
+              onClick={rewind}
+            />
+            <h2 className="text-12 font-bold text-white-4">-5</h2>
+          </div>
+          <Image
+            src={isPlaying ? "/icons/Pause.svg" : "/icons/Play.svg"}
+            width={30}
+            height={30}
+            alt="play"
+            onClick={togglePlayPause}
+          />
+          <div className="flex items-center gap-1.5">
+            <h2 className="text-12 font-bold text-white-4">+5</h2>
+            <Image
+              src={"/icons/forward.svg"}
+              width={24}
+              height={24}
+              alt="forward"
+              onClick={forward}
+            />
+          </div>
+        </div>
+        <div className="flex items-center gap-6">
+          <h2 className="text-16 font-normal text-white-2 max-md:hidden">
+            {formatTime(duration)}
+          </h2>
+          <div className="flex w-full gap-2">
+            <Image
+              src={isMuted ? "/icons/unmute.svg" : "/icons/mute.svg"}
+              width={24}
+              height={24}
+              alt="mute unmute"
+              onClick={toggleMute}
+              className="cursor-pointer"
+            />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default PodcastPlayer;
+ 
